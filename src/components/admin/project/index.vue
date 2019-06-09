e0b65b4f93758580286d4f4017e77b7fe293cb46
zhangye <80447583@qq.com>
2019-06-09 23:47:38 +0800

    优化细节

--- src/components/admin/project/index.vue
+++ src/components/admin/project/index.vue
@@ -21,7 +21,7 @@
         <el-form-item label="项目状态">
           <el-select v-model="search.state" placeholder="项目状态">
             <el-option label="全部" value></el-option>
-            <el-option label="未验收" value="0"></el-option>
+            <el-option label="待验收" value="0"></el-option>
             <el-option label="已验收" value="1"></el-option>
           </el-select>
         </el-form-item>
